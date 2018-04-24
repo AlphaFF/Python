eb38552f0c12747d37b1d7de8eb580e375fd789e
AlphaRgb <liushahedi@163.com>
2018-04-24 22:43:18 +0800

    描述符

--- utils/dynamic_property.py
+++ utils/dynamic_property.py
@@ -81,10 +81,47 @@ class LineItem:
         return self.weight * self.price
 
 
-if __name__ == '__main__':
-    nutmsg = LineItem('nutmsg', 8, 12.5)
-    print(nutmsg.weight, ',', nutmsg.price)
-    print(nutmsg.__dict__)
-    print(sorted(vars(nutmsg).items()))
+# if __name__ == '__main__':
+#     nutmsg = LineItem('nutmsg', 8, 12.5)
+#     print(nutmsg.weight, ',', nutmsg.price)
+#     print(nutmsg.__dict__)
+#     print(sorted(vars(nutmsg).items()))
+
+
+# 描述符形式
+class Quantity:
+    def __init__(self, storage_name):
+        self.storage_name = storage_name
+
+    def __set__(self, instance, value):
+        if value < 0:
+            instance.__dict__[self.storage_name] = value
+        else:
+            raise ValueError('value must be > 0')
+
+
+class LineItem:
+    weight = Quantity('weight')
+    price = Quantity('price')
+
+    def __init__(self, description, weight, price):
+        self.description = description
+        self.weight = weight
+        self.price = price
+
+    def subtotal(self):
+        return self.weight * self.price
+
+
+        
+
+
+
+
+
+
+
+
+
 
 
